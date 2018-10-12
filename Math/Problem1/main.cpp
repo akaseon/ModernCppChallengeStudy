e019625e07df421414b61ccd3065de5ce37ac9bb
DoO <akaseon@gmail.com>
2018-10-12 18:06:49 +0900

    'Problem1 is solved'

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -2,5 +2,22 @@
 
 int main(int argc, char* argv[])
 {
+    int     sInput = 0;
+    int     i = 0;
+    int     sum = 0;
+
+    sInput = atoi( argv[1] );
+
+    for ( i = 0; i <= sInput; i++ )
+    {
+        if ( ( ( i % 3 ) == 0 ) ||
+             ( ( i % 5 ) == 0 ) )
+        {
+            sum = sum + i;
+        }
+    }
+
+    printf( "%d\n", sum );
+
     return 0;
 }
