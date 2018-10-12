3f41b775c5ad434701417fa5faa099afebbd3fc8
DoO <akaseon@gmail.com>
2018-10-12 18:07:12 +0900

    Problem2 is been solving..

--- Math/Problem2/main.cpp
+++ Math/Problem2/main.cpp
@@ -1,6 +1,40 @@
 #include <gsl/gsl>
 
+int getGcd( int aNumber1, int aNumber2 )
+{
+    int     sRemainder = 0;
+    int     sQuotient = 0;
+
+    if ( aNumber1 > aNumber2 )
+    {
+        sQuotient = sNumber1 / sNumber2;
+        sRemainder = sNumber1 - ( sNumber2 * sQuotient );
+    }
+    else
+    {
+        sQuotient = sNumber2 / sNumber1;
+        sRemainder = sNumber2 - ( sNumber1 * sQuotient );
+    }
+
+    if ( sRemainder == 0 )
+    {
+    }
+    else
+    {
+        return sGcd;
+
+    }
+
+
+}
+
 int main(int argc, char* argv[])
 {
+    int num1 = 0;
+    int num2 = 0;
+
+    num1 = atoi( argv[1] );
+    num2 = atoi( argv[2] );
+
     return 0;
 }
