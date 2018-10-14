454a489965451cadf5ebbea6a24c0832ad902744
DoO <akaseon@gmail.com>
2018-10-14 11:28:24 +0900

    Problem8 is solved

--- Math/Problem8/main.cpp
+++ Math/Problem8/main.cpp
@@ -1,6 +1,54 @@
 #include <gsl/gsl>
 
+#include <cmath>
+#include <iostream>
+
+int getDigitNumber( int aNumber )
+{
+    int sDigitNumber = 0;
+    int sNumber = aNumber;
+
+    do {
+        sDigitNumber++;
+        sNumber = sNumber / 10;
+    } while ( sNumber != 0 );
+
+    return sDigitNumber;
+}
+
+double getArmstrongNumber( int aNumber )
+{
+    int     sDigitValue = 0;
+    int     sDigitNumber = 0;
+    int     sNumber = aNumber;
+    double  sArmstrongNumber = 0;
+
+    sDigitNumber = getDigitNumber( aNumber );
+
+    do {
+        sDigitValue = sNumber % 10;
+        sNumber = sNumber / 10;
+        sArmstrongNumber += std::pow( sDigitValue, sDigitNumber );
+    } while ( sNumber != 0 );
+
+    return sArmstrongNumber;
+}
+
 int main(int argc, char* argv[])
 {
+    int sNumber = 0;
+
+    sNumber = atoi( argv[1] );
+
+    if ( sNumber == getArmstrongNumber( sNumber ) )
+    {
+        std::cout << sNumber << " is ArmstrongNumber\n" ;
+    }
+    else
+    {
+        std::cout << sNumber << " is not ArmstrongNumber\n" ;
+    }
+
+
     return 0;
 }
