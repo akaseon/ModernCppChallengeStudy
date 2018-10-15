846c0ecc9eb0ec3e718375f494d287c19f5d4386
DoO <akaseon@gmail.com>
2018-10-15 19:43:06 +0900

    Problem14 is solved

--- Math/Problem14/main.cpp
+++ Math/Problem14/main.cpp
@@ -1,6 +1,57 @@
 #include <gsl/gsl>
 
+#include <cstdlib>
+#include <iostream>
+
+bool isValildISBN( std::string  aISBN )
+{
+    char    sCheckDigit;
+    int     sCheckDigitNumber = 0;
+    int     sWeightSum = 0;
+    bool    sIsValid = false;
+
+    sCheckDigit = aISBN[9];
+
+    if ( ( sCheckDigit == 'x' ) ||
+         ( sCheckDigit == 'X' ) )
+    {
+        sCheckDigitNumber = 10;
+    }
+    else
+    {
+        sCheckDigitNumber = sCheckDigit - '0';
+    }
+
+    sWeightSum = sCheckDigitNumber;
+
+    for ( int i = 0; i < aISBN.length() - 1; i++ )
+    {
+        sWeightSum = sWeightSum + ( ( aISBN[i] - '0' ) * ( 10 - i ) );
+    }
+
+    if ( ( sWeightSum % 11 ) == 0 )
+    {
+        sIsValid = true;
+    }
+    else
+    {
+        sIsValid = false;
+    }
+
+    return sIsValid;
+}
+
+
 int main(int argc, char* argv[])
 {
+    if ( isValildISBN( argv[1] ) == true )
+    {
+        std::cout << argv[1] << " is valid\n";
+    }
+    else
+    {
+        std::cout << argv[1] << " is invalid\n";
+    }
+
     return 0;
 }
