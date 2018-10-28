7203c01c48d99048ee4d1e912bd0c431385a2db5
DoO <akaseon@gmail.com>
2018-10-28 14:24:07 +0900

    23 ...

--- Regex/Problem23/main.cpp
+++ Regex/Problem23/main.cpp
@@ -1,6 +1,35 @@
-#include <gsl/gsl>
+//#include <gsl/gsl>
 
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+std::string convertString( std::vector<int> aFrom )
+{
+
+    std::ostringstream sStringStream;
+
+    for ( auto sIt = aFrom.begin();
+          sIt != aFrom.end();
+          sIt++ )
+    {
+        //std::cout << std::hex < *sIt << " ";
+        sStringStream << std::setfill( '0' ) << std::setw(2) << std::hex << *sIt;
+        //sStringStream << *sIt;
+    }
+
+    std::cout << sStringStream.str() << "\n";
+
+    return sStringStream.str();
+}
 int main(int argc, char* argv[])
 {
+    std::vector<int>    sVector;
+
+    sVector.push_back( 10 );
+    sVector.push_back( 11 );
+    sVector.push_back( 12 );
+    convertString( sVector );
     return 0;
 }
